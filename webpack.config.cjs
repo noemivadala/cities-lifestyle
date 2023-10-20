c68a3af26c4274715e7f951b156e8f8890e5dd55
noemi.vadala.nv@gmail.com <noemi.vadala.nv@gmail.com>
2023-10-20 10:15:51 +0200

    cambiato output

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -9,7 +9,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js',
+        filename: '[name].[contenthash].js',
         clean: true,
     },
     module: { rules: [
