5b9e757bad00aa7fa6f6650e52732203083fde50
noemi.vadala.nv@gmail.com <noemi.vadala.nv@gmail.com>
2023-10-23 15:41:46 +0200

    autocomplete

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,15 @@
 import '../sass/style.scss';
 import axios from 'axios';
-import { containerCard, search, searchValue, textError, iconClose } from "./modules/variables.js";
-import { clearField, findCity, findCityEnter } from "./modules/functions.js";
+var _ = require('lodash');
+import { sectionSearch, containerCard, search, searchLabel, searchValue, textError, iconClose } from "./modules/variables.js";
+import { clearField, findCity, findCityEnter, createElement } from "./modules/functions.js";
 import { cardCity } from "./component/card.js";
 
+let containerListCity = createElement('div', 'containerListCity', 'list-city');
+sectionSearch.appendChild(containerListCity);
+
 let urlCity = "https://api.teleport.org/api/urban_areas/slug:";
+let urlListCities = "https://api.teleport.org/api/urban_areas/";
 
 //call
 async function getData(searchTerm) {
@@ -29,10 +34,66 @@ async function getData(searchTerm) {
     });
 }
 
+let cities = [];
+
+async function getListCity() {
+  try {
+    const response = await axios.get(urlListCities);
+    cities = _.get(response, "data._links.ua:item", []);
+
+    cities.forEach(city => {
+      const name = city.name;
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+searchValue.onkeyup = function() {
+  let result = [];
+  let input = searchValue.value;
+  console.log(input);
+  if(input.length){
+    result = cities.filter((city) => {
+      if (city.name && typeof city.name === 'string') {
+        return city.name.toLowerCase().includes(input);
+      }
+      return false;
+    });
+  }
+  previewCity(result);
+}
+
+function previewCity(result) {
+  const content = result.map((city) => {
+    const listItem = document.createElement('li');
+    listItem.textContent = city.name;
+    console.log(city);
+    listItem.addEventListener('click', () => selectInput(city));
+    return listItem;
+  });
+
+  const ul = document.createElement('ul');
+  ul.append(...content);
+
+  containerListCity.innerHTML = '';
+  containerListCity.appendChild(ul);
+}
+
+function selectInput(city) {
+  searchValue.value = city.name;
+  containerListCity.innerHTML = '';
+  getData(searchValue.value);
+}
+
+
 //event button
-search.addEventListener("click", getData);
-iconClose.addEventListener("click", clearField);
-searchValue.addEventListener("keyup", findCity);
-searchValue.addEventListener("keydown", findCityEnter);
+search.addEventListener('click', getData);
+iconClose.addEventListener('click', clearField);
+searchValue.addEventListener('keyup', findCity);
+searchValue.addEventListener('keydown', findCityEnter);
+searchValue.addEventListener('click', getListCity);
+
 
 export { getData };
--- src/js/modules/variables.js
+++ src/js/modules/variables.js
@@ -1,8 +1,10 @@
-export { containerCard, containerScore, search, searchValue, textError, iconClose };
+export { sectionSearch, containerCard, containerScore, search, searchLabel, searchValue, textError, iconClose };
 
+const sectionSearch = document.getElementById("sectionSearch");
 const containerCard = document.getElementById("containerCard");
 const containerScore = document.getElementById("containerScore");
 const search = document.getElementById("btn-search");
+const searchLabel = document.getElementById("search");
 const searchValue = document.getElementById("valueLabel");
 const textError = document.getElementById("textError");
 const iconClose = document.getElementById("icon-close");
